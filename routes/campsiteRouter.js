127682dfba2ebd08077e4a3610851aead75adee4
yimxx042 <79169462+yimxx042@users.noreply.github.com>
2021-06-16 12:33:39 -0500

    add promotion and partner

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -21,4 +21,36 @@ campsiteRouter.route('/')
     res.end('Deleting all campsites');
 });
 
+campsiteRouter.route('/:campsiteId')
+
+.all((req, res, next) => {
+    res.status = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res) => {
+    res.end(`will send campsite ${req.params.campsiteId} to you`)
+})
+
+//POST
+
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`Post operation not supported on /campsite/${req.params.campsiteId}`);
+})
+
+//PUT 
+.put( (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`)
+    res.end(`Will update the campsite: ${req.body.name}
+           with description: ${req.body.description}`)
+})
+
+//delete
+.delete( (req, res) => {
+    res.end(`Deleting the campsite ${req.params. campsiteId}`)
+})
+
+
 module.exports = campsiteRouter;
